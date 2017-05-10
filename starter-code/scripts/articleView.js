37956a619518729eca6efdf3a20932ce5e850ad7
Justin Green <justin98366@gmail.com>
2017-05-10 13:47:52 -0700

    got a lot of funtionality done with our event handlers

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -36,14 +36,19 @@ articleView.handleAuthorFilter = function() {
     // REVIEW: Inside this function, "this" is the element that triggered the event handler function we're
     //         defining. "$(this)" is using jQuery to select that element, so we can chain jQuery methods
     //         onto it.
+    $('article').hide();
     if ($(this).val()) {
       // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
 
+      var authorName = $(this).val();
+      $('article[data-author="' + authorName + '"]').fadeIn(750);
+
     } else {
       // TODO: If the select box was changed to an option that is blank, we should
       //       show all the articles, except the one article we are using as a template.
+      $('article:not(.template)').fadeIn(750);
 
     }
     $('#category-filter').val('');
@@ -84,5 +89,6 @@ articleView.setTeasers = function() {
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
-
+  articleView.populateFilters();
+  articleView.handleAuthorFilter();
 })
